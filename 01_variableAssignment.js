f6d06cbe4577e59761cf2007b40d0a5c6fbff36b
omkarbhosale1991 <omkar.bhosale911991@gmail.com>
2023-02-16 21:17:04 +0530

    01_variableAssignment
    01_variableAssignment

---
+++ 01_variableAssignment.js
@@ -0,0 +1,32 @@
+
+console.log("-------Step 1--------");
+console.log(myFullName);
+console.log("-------Step 2--------");
+var myFullName;
+console.log("myFullName");
+
+myFullName="Omkar Bhosale";
+console.log(myFullName);
+
+myFullName="Omkar Annasaheb Bhosale";
+console.log(myFullName);
+console.log("-------Step 3--------");
+var myLove="I love only JS";
+console.log("I love only JS");
+console.log("-------Step 4--------");
+var firstName="Omkar";
+console.log("firstName:",firstName);
+var lastName="Bhosale"
+console.log("lastName:",lastName);
+var age="31";
+console.log("age:",age);
+var address= "11k county Moshi,Pune";
+console.log("address:",address);
+var pincode="512105";
+console.log("pincode:", pincode);
+var gender="Male";
+console.log("Gender:",gender);
+var isMarried="No";
+console.log("isMarried:",isMarried);
+
+
