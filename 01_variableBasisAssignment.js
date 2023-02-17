185016bb20107e23a348b50001267baaa40e2020
omkarbhosale1991 <omkar.bhosale911991@gmail.com>
2023-02-17 22:35:36 +0530

    01_variablebasisAssignment
    01_variablebasisAssignment
    02_typeof

---
+++ 01_variableBasisAssignment.js
@@ -0,0 +1,32 @@
+// Create three variable of each data type
+console.log(".........num data type.........");
+var rollNo = 3322;
+console.log("Roll No. is:",rollNo," And Data type is:",typeof rollNo);
+var roomNo=804;
+console.log("Room No. is:",roomNo,"And Data type is",typeof roomNo);
+var bikeNo=007;
+console.log("Bike No Plate is:",bikeNo,"Data type of bikeNo id:",typeof bikeNo);
+console.log("---------- Datatype  string----------");
+//String name= Omkar_Bhosale;
+var bikeNo="NH 09 OM 007";
+console.log("Bike No Plate is:",bikeNo," And Data type of bikeNo id:",typeof bikeNo);
+var nameOfCandidate="Omkar Bhosale.";
+console.log("name of student",nameOfCandidate,"Data type of name is",typeof nameOfCandidate);
+var shoolName="KVM,Moor";
+console.log("10th Shool is:",shoolName,"Data type of schoolMane is:",typeof shoolName);
+var diplomaCollege="AIT,Vita";
+console.log("diploma complated from:",diplomaCollege,"Data type of diplomaCollege",typeof diplomaCollege);
+console.log("--------Datatype Boolean----------");
+var blogging=true;
+console.log("Data type of blogging is:",typeof blogging);
+var friend=false;
+console.log("Data type of friend is:",typeof friend);
+console.log("--------------Diff. between null and undefine----------------");
+var diff="(1)undefine contain value and null is contain no value (2) null is emty and undine contain udefine value";
+console.log("Diff. between null and undefine",diff);
+
+
+
+
+
+
---
+++ 02_typeof.js
@@ -0,0 +1,22 @@
+console.log("--------Step 1-------");
+var bankName;
+console.log(bankName,"Data type of SBI bank is:",typeof bankName);
+var bankName='SBI Bank';
+console.log(bankName,"Data type of SBI Bank is:",typeof bankName);
+console.log("---------step 2----------");
+var marks='90%';
+console.log("marks is:",marks,"data type of '90%' is:",typeof marks);
+console.log("---------step 3----------");
+var isWorking;//variable is Declation
+console.log(isWorking);
+isWorking=true;//variable initialization
+console.log("value of isWorking is",isWorking," And data type of isWorking",typeof isWorking);
+console.log("---------step 4----------");
+var totalCount="Hundred and seven";
+console.log("Data type is",typeof totalCount);
+totalCount=107;
+console.log("Total count is",totalCount,"Data type of updated value of count",typeof totalCount);
+
+
+
+
