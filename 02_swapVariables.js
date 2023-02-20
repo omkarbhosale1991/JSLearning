faef98848b3740b056437f811ecbe47b0a4c94c4
omkarbhosale1991 <omkar.bhosale911991@gmail.com>
2023-02-20 18:01:44 +0530

    updated swappingAssignment
    trubbbleing with 3 variable swapping

--- 02_swapVariables.js
+++ 02_swapVariables.js
@@ -24,18 +24,20 @@
 
 console.log("--------Step 1 ----------");
 console.log("swipping variable");
-var str1="Sweety";
-var str2="Cutie";
-var temp=ste1;
-str1=str2;
-str2=temp;
-console.log(str1,str2);
+var str1 = "Sweety"; //step1
+var str2 = "Cutie"; //step1
+var temp = str1; //step 2
+str1 = str2; //step 3
+str2 = temp; //step 4
+console.log(str1, str2); //step 5
 console.log("----------Step 2---------");
-var num1=100;//200
-var num2=200;//300
-var num3=300;//100
-temp=num1;
-num1=num2;
-num2=num3;
-temp=num1;
-console.log("Swapping value of number is:",num1,num2,num3);
+var num1 = 100; //200
+var num2 = 200; //300
+var num3 = 300; //100
+console.log("value before swap is", num1, num2, num3);
+var temp = num1;
+num1 = num2;
+num2 = temp;
+num2 = num3;
+num3 = temp;
+console.log("Swapping value of number is:", num1, num2, num3);
