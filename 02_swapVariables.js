25d9dce250a7e7a920437dc4b6760dd8e63ef87c
omkarbhosale1991 <omkar.bhosale911991@gmail.com>
2023-02-20 17:16:23 +0530

    swappingAssigment
    Swapping assignment but show uncaught reference error

---
+++ 02_swapVariables.js
@@ -0,0 +1,41 @@
+// console.log("----------------Step 1--------------");
+// -------------this is wrong code------------
+// var sweety="Sweety";
+// console.log(sweety);
+// var cutie="Cutie";
+// console.log(cutie);
+// var temp= sweety;
+// sweety=cutie;
+// console.log(temp);
+// cutie=temp;
+// console.log(temp);
+
+// console.log("------------step 2-------------");
+// var num1= 100;
+// console.log("num1 value is:",num1);
+// var num2=200;
+// console.log("num2 value is:",num2);
+// var num3=300;
+// console.log("num3 value is:",num3);
+// num1=num2;
+// num2=num3;
+// num3=temp2;
+// console.log("temp2 file is",temp2);
+
+console.log("--------Step 1 ----------");
+console.log("swipping variable");
+var str1="Sweety";
+var str2="Cutie";
+var temp=ste1;
+str1=str2;
+str2=temp;
+console.log(str1,str2);
+console.log("----------Step 2---------");
+var num1=100;//200
+var num2=200;//300
+var num3=300;//100
+temp=num1;
+num1=num2;
+num2=num3;
+temp=num1;
+console.log("Swapping value of number is:",num1,num2,num3);
