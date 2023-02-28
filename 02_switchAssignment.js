4e958d0125ef00ff435cbe15b424ffb56ee14a2a
omkarbhosale1991 <omkar.bhosale911991@gmail.com>
2023-02-28 23:55:25 +0530

    updated
    updated

--- 02_switchAssignment.js
+++ 02_switchAssignment.js
@@ -3,6 +3,12 @@
 // Year is multiple of 4 and not multiple of 100.
 
 var check_leap_Year = function (leap_year) {
+    //unhappy path
+    if (leap_year== null ||leap_year==undefined||leap_year===NaN ) 
+    {
+    console.log(`Please re-enter Year correctly`);    
+    }
+    
   if ( leap_year % 4 == 0 && leap_year % 100 != 0 || leap_year % 400 == 0 )
   {
     console.log(`${leap_year} is leap year`);
@@ -12,9 +18,13 @@ var check_leap_Year = function (leap_year) {
     console.log(`${leap_year} is not leap year`);
   }
 };
-check_leap_Year(2020)
-check_leap_Year(2021);
+check_leap_Year(2020);
+check_leap_Year(1999);
+check_leap_Year(1600);
 check_leap_Year(2022);
-check_leap_Year(2023);
-check_leap_Year(2024);
-check_leap_Year(2025);;
+check_leap_Year(1945);
+check_leap_Year(null);
+check_leap_Year("twenty twenty");
+check_leap_Year(undefined);
+check_leap_Year(NaN);
+check_leap_Year(1750);
