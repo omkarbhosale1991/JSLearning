f99b06b7391f4d79b80b8be56bcc37fae9d46916
omkarbhosale1991 <omkar.bhosale911991@gmail.com>
2023-03-06 22:56:26 +0530

    ArrayAssigA
    ArrayAssigA, replacing orange with kivi ?

---
+++ 08_arrayAssigA.js
@@ -0,0 +1,49 @@
+var arraySeasonalFruits=["Banana","Orange","Apple","Mango","Water melon"]
+console.log(`============ log first and last element ============`);
+console.log(arraySeasonalFruits);
+arraySeasonalFruits.push();
+console.log(`Last element of array is ==>> ${arraySeasonalFruits}`);
+console.log(`================ last element==============`);
+console.log(arraySeasonalFruits);
+
+console.log(`========== add  element in first place ===============`);
+console.log(arraySeasonalFruits);
+arraySeasonalFruits.unshift("Papaya");
+console.log(`Papaya add in first place ==>> ${arraySeasonalFruits}`);
+
+console.log(`========== Remove element in last place ===============`);
+console.log(arraySeasonalFruits);
+arraySeasonalFruits.pop();
+console.log(`remove element in last place from array ==>> ${arraySeasonalFruits}`);
+
+console.log(`========== Add  element in last place ===============`);
+console.log(arraySeasonalFruits);
+arraySeasonalFruits.push("Pineapple")
+console.log(` add  element in last place is : ${arraySeasonalFruits}`);
+
+console.log(`========== insert a element without replacing ===============`);
+
+console.log(arraySeasonalFruits);
+arraySeasonalFruits.splice(4,0,"Dragon fruit")
+console.log(`Insert dragon fruit before water melon ==>> ${arraySeasonalFruits}`);
+
+console.log(`========== replace element  ===============`);
+var arraySeasonalFruits=["Banana","Orange","Apple","Mango","Water melon"]
+console.log(`Before replacing element is==>> ${arraySeasonalFruits}`);
+const kivi=arraySeasonalFruits.slice(1,"Orange","Kivi");
+console.log(`Replace Orange with Kivi is: ${kivi}`);
+
+// arraySeasonalFruits.replace("Orange","Kivi");// ------------wrong logic
+
+
+console.log(`==================== Log a element 1 to 4 index ==================== `); 
+var arraySeasonalFruits=["Banana","Orange","Apple","Mango","Water melon"] 
+console.log(arraySeasonalFruits);
+const subarray1To4=arraySeasonalFruits.slice(1,4)
+console.log(`index position 1 to 4 is ==>> ${subarray1To4}`);
+
+console.log(`==================== Select last 3 element ==================== `); 
+console.log(arraySeasonalFruits);
+const n = 3;
+const last3Ele = arraySeasonalFruits.slice
+console.log(`Select last 3 element is ==>> ${last3Ele}`);
