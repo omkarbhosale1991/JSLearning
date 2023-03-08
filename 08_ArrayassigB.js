ace4e8dc6120a22a8549ec0c0d27f629d3d3778f
omkarbhosale1991 <omkar.bhosale911991@gmail.com>
2023-03-09 02:52:44 +0530

    upadate
    upadate

--- 08_ArrayassigB.js
+++ 08_ArrayassigB.js
@@ -1,61 +1,49 @@
 console.log(`================= Find total element ===============`);
 const arrayNumbers = [ 20, 31, 40, 25, 23, 11, 29, 9, 60, 2, 11 ];
+console.log(arrayNumbers);
 console.log(`Length of array is :${arrayNumbers.length}`);
 console.log(`================== first element and last element ==============`);
+console.log(arrayNumbers);
 const valueAtIndex0=arrayNumbers[0]
 console.log(`first element using (const valueAtIndex0=arrayNumbers[0]) is ${valueAtIndex0}`);
+console.log(arrayNumbers);
 const firstEle=arrayNumbers.shift()
 const lastEle =arrayNumbers.slice(-1)
 console.log(`first element (using shift) And last element is ${firstEle},${lastEle}`);
-// console.log(`first element And last element is ${firstEle,lastEle}`);// wronng
 
 console.log(`============ third last element using length property==============`);
-// const last3rdEle =arrayNumbers.slice(-3)
-// // console.log(`third last element using length property is ${last3rdEle}`); wrong
-// const last3rdElement=arrayNumbers.length-3;
-// console.log(`third last element using length property is ${last3rdElement}`); // wrong
-
+console.log(arrayNumbers);
 const last3rdElement=arrayNumbers[arrayNumbers.length-3]; 
-console.log(`third last element using length property is ${last3rdElement}`);
+console.log(` Find out value of third last element using length property is ${last3rdElement}`);
 
 console.log(`================= all even numbers in array ===============`);
 console.log(arrayNumbers);
-const evenNumber=[]
+let sumOfEvn=0;
 for (let index = 0; index < arrayNumbers.length; index++) {
     const num = arrayNumbers[index];
     if (num %2==0) {
-        
-        evenNumber.push(num)
+        console.log(`Even no. in array is ${num}`);
+       sumOfEvn= sumOfEvn+ num;
     }  
 }
-console.log(`Even no. in array is ==>> ${evenNumber}`);
-//..........................................................................................
-
-// // let evenNum; // declaration
-// let evenNum=[];
-// for (let index = 0; index < arrayNumbers.length; index++) {
-//   if (arrayNumbers[index]%2==0) {
-//    evenNum.push(arrayNumbers[index]); // evenNum.unshift[arrayNumbers] can use but slow to process of program
-    
-//   }
-// }
-// console.log(`all even numbers in array is ${evenNum}`);
+console.log(`total sum of even number is ${sumOfEvn}`);
 
 console.log(`================= all odd numbers in array ===============`);
 console.log(arrayNumbers);
-const oddNummber=[]
+let sumOfOdd = 0;
 for (let index = 0; index < arrayNumbers.length; index++) {
-    const num = arrayNumbers[index];
-    if (num %2==1) {
-        
-        oddNummber.push(num)
-    }  
+  const num = arrayNumbers[index];
+  if (num % 2 == 1) {
+    console.log(`Odd no. in array is ${num}`);
+    sumOfOdd = sumOfOdd + num;
+  }
 }
-console.log(`Odd number in array is ==>> ${oddNummber}`);
+console.log(`total sum of odd number is ${sumOfOdd}`);
 
 
 
 console.log(`================= Sum of all even numbers index in array ===============`);
+console.log(arrayNumbers);
 const evenNumb=[];
 let sum=0;
 for (let index = 0; index < arrayNumbers.length; index++) {
@@ -72,8 +60,11 @@ evenNumb.push(sum)
 console.log(`Sum of all even numbers index in array ${sum}`);
 
 console.log(`================= Sum of all odd numbers index in array ===============`);
+console.log(arrayNumbers);
 console.log(`Sum of all odd numbers index in array is:`);
+console.log(arrayNumbers);
 console.log(`================= Sum of all element in array ===============`);
+console.log(arrayNumbers);
 console.log(`Sum of all element in array is:`);
 
 // console.log(arrayNumbers);
@@ -84,7 +75,7 @@ console.log(`Sum of all element in array is:`);
     
 // }
 console.log(`================= Numbers which are multiple of 5 ==========================`);
-
+console.log(arrayNumbers);
 console.log(`================== Is number 115 available in arrayNumbers ? ==================`);
 console.log(arrayNumbers);
 let availableOrNot=arrayNumbers.includes(115);
