0449a8e796b3a032788b768d507b497eb5f92d27
omkarbhosale1991 <omkar.bhosale911991@gmail.com>
2023-03-17 06:28:44 +0530

    update
    updated

--- fibonacciSeries.js
+++ fibonacciSeries.js
@@ -1,8 +1,9 @@
 const fabonacci= function fabonacciSeries(num) {
-    for (let index = 0; index < num; index++) {
+    for (let index = 0; index <= num; index++) {
     
-        let num1=0+num;
-       const result= num1++
+        let num1=0+1+num;
+        let num2= num1++
+       const result=num2
        return result
     }
 
