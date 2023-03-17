745f3def3c3dcb63551909aeb9c796aa263ffd9b
omkarbhosale1991 <omkar.bhosale911991@gmail.com>
2023-03-17 06:17:37 +0530

    Create fibonacciSeries.js

---
+++ fibonacciSeries.js
@@ -0,0 +1,13 @@
+const fabonacci= function fabonacciSeries(num) {
+    for (let index = 0; index < num; index++) {
+    
+        let num1=0+num;
+       const result= num1++
+       return result
+    }
+
+
+    
+}
+const result=fabonacci(10);
+console.log(result);
